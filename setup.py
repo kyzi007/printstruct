47f6e5515fb70f622e1750a1b69342b02193ce2b
kyzi007 <kyzi007@rambler.ru>
2014-04-06 11:19:51 +0300

    try pack

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='printstruct',
+    version='0.1',
+    packages=['printstruct'],
+    url='',
+    license='',
+    author='lena',
+    author_email='',
+    description=''
+)
